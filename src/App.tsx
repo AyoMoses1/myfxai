8b6aae325eee03a07ef2931f90dedd8691fb18fe
Ayo Moses <ayomoses111@gmail.com>
2023-06-16 12:49:12 +0100

    Work on app adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from './theme';
-import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/main';
 import Terms from './pages/terms'
 import Policies from './pages/policies'
