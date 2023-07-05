028972430b705f8d9e7ab496832aa17ac8c3728e
Ayo Moses <ayomoses111@gmail.com>
2023-07-05 13:52:50 +0100

    Fix night mode for faq page

--- src/pages/faq/components/jumbotron.tsx
+++ src/pages/faq/components/jumbotron.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import { ArrowForwardIcon } from '@chakra-ui/icons';
-import { Box, Button, Text, Image, HStack } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Text,
+  Image,
+  HStack,
+  useColorMode,
+  border,
+} from '@chakra-ui/react';
 import styled from 'styled-components';
 import a1 from 'assets/avatars/a1.png';
 import a2 from 'assets/avatars/a2.png';
 import a3 from 'assets/avatars/a3.png';
 
 const Jumbotron = () => {
+  const { colorMode } = useColorMode();
+  const bgColor = colorMode === 'dark' ? 'gray.800' : '#F9FAFB';
+  const borderColor = colorMode === 'dark' ? '#333B43' : '#000';
+
   return (
-    <StyledBox bg="#F9FAFB" p={8} rounded="md" textAlign="center">
+    <StyledBox
+      bg={bgColor}
+      p={8}
+      rounded="md"
+      textAlign="center"
+      borderColor={borderColor}
+    >
       <HStack
         justify="center"
         mb={4}
@@ -18,7 +36,13 @@ const Jumbotron = () => {
         my={8}
       >
         <Image src={a1} position="absolute" right="100px" zIndex={1} />
-        <Image src={a2} position="absolute" right="50px" top="-38px" zIndex={3} />
+        <Image
+          src={a2}
+          position="absolute"
+          right="50px"
+          top="-38px"
+          zIndex={3}
+        />
         <Image src={a3} position="absolute" right="12px" zIndex={1} />
       </HStack>
       <Text variant="faqQuestion" mb={4}>
@@ -44,5 +68,6 @@ const StyledBox = styled(Box)`
   border-image-width: 1px;
   border-image-outset: 0;
   border-image-repeat: stretch;
-  border-image: linear-gradient(to top, #000 28.27%, rgba(0, 0, 0, 0.1) 100%) 1;
+  border-image: ${(props) =>
+    `linear-gradient(to top, ${props.borderColor} 28.27%, rgba(0, 0, 0, 0.1) 100%) 1`};
 `;
